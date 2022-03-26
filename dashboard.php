7b61d538da41a09108a0a0126074267810455dff
HarryKing87 <babishh96@hotmail.com>
2022-03-27 00:37:34 +0200

    Added Mobile Version for dashboard with Tailwind

--- dashboard.php
+++ dashboard.php
@@ -13,17 +13,72 @@ if(!$_SESSION['username'])
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="CSS/dashboard.css">
-    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Dashboard | Customer Expenses</title>
 </head>
 <body>
-    <div class="container">
-            <li><a href="dashboard.php">Show Customers</a></li>
-            <li><a href="newCustomer.php">Insert Customers</a></li>
-            <li><a href="expenses.php">Customer Expenses</a></li>
-            <li><a href="addExpenses.php">Add Expenses</a></li>
-            <li name="logout"><a href="logout.php">Logout</a></li>
-    </div>
+    
+<nav class="bg-black-500 shadow-lg dark:bg-gray-800">
+			<div class="max-w-6xl mx-auto px-4">
+				<div class="flex justify-between">
+					<div class="flex space-x-7">
+						<div>
+							<!-- Website Logo -->
+							<a href="dashboard.php" class="flex items-center py-4 px-2">
+								<span class="font-semibold text-lg  text-black dark:text-white">Customer Expenses</span>
+							</a>
+						</div>
+						<!-- Primary Navbar items -->
+						<div class="hidden md:flex items-center space-x-1">
+							<a href="dashboard.php" class="py-4 px-2 text-green-500 border-b-4 border-green-500 font-semibold ">Home</a>
+							<a href="newCustomer.php" class="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300">Add Customer</a>
+							<a href="expenses.php" class="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300">Customer Expenses</a>
+							<a href="addExpenses.php" class="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300">Add Expenses</a>
+						</div>
+					</div>
+					<!-- Secondary Navbar items -->
+					<div class="hidden md:flex items-center space-x-3 " name = "logout">
+						<a href="logout.php" class="py-2 px-2 font-medium text-white bg-green-500 rounded hover:bg-green-400 transition duration-300">Logout</a>
+					</div>
+					<!-- Mobile menu button -->
+					<div class="md:hidden flex items-center">
+						<button class="outline-none mobile-menu-button">
+						<svg class=" w-6 h-6 text-black dark:text-white hover:text-green-500 "
+							x-show="!showMenu"
+							fill="none"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							viewBox="0 0 24 24"
+							stroke="currentColor"
+						>
+							<path d="M4 6h16M4 12h16M4 18h16"></path>
+						</svg>
+					</button>
+					</div>
+				</div>
+			</div>
+			<!-- mobile menu -->
+			<div class="hidden mobile-menu">
+				<ul class="">
+					<li class="active"><a href="dashboard.php" class="block text-sm px-2 py-4 text-white bg-green-500 font-semibold">Home</a></li>
+					<li><a href="newCustomer.php" class="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">Add Customer</a></li>
+					<li><a href="expenses.php" class="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">Customer Expenses</a></li>
+					<li><a href="addExpenses.php" class="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">Add Expenses</a></li>
+					<li><a href="logout.php" class="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">Logout</a></li>
+                </ul>
+			</div>
+			<script>
+				const btn = document.querySelector("button.mobile-menu-button");
+				const menu = document.querySelector(".mobile-menu");
+
+				btn.addEventListener("click", () => {
+					menu.classList.toggle("hidden");
+				});
+			</script>
+		</nav>
+
+
 <div data-aos="fade-up" data-aos-duration="1500">
     <!--
     <button id="arrowDown">Sort Values</button>
@@ -104,7 +159,7 @@ if(!$_SESSION['username'])
     // --- END OF SORTING CODE
     $sql = "SELECT * FROM CUSTOMERS ORDER BY address ASC";
     $result = $db->query($sql);
-    echo "<table>
+    echo "<table class='table-auto'>
     <tr>
     <th>ID</th>
     <th>NAME</th>
@@ -136,10 +191,6 @@ if(!$_SESSION['username'])
     xhttp.open('POST', "dashboard.php", true);
     xhttp.setRequestHeader('Content-Type', 'application/');
     xhttp.send();
-</script>
-<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-<script>
-  AOS.init();
 </script>
 </div>
 </body>
